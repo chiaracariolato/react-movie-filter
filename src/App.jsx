1ef0d952e7a3188cf551fddfaad0b22370a4017d
chiaracariolato <chiaracariolato@gmail.com>
2026-04-15 18:51:31 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>CIao</h1>
+      <button className="btn btn-primary">PROVA</button>
+    </>
+  )
+}
+
+export default App
